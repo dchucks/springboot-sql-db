b1130e61dfb421b29d3ec38f5a5474c75f1a70b6
Debashish <debashish@gmail.com>
2018-06-20 21:14:21 +0530

    Added Id attribute

--- src/main/java/net/javabeat/Person.java
+++ src/main/java/net/javabeat/Person.java
@@ -1,28 +1,34 @@
 package net.javabeat;
 
 public class Person {
+
+  private int id;
   private String firstName;
   private String lastName;
   private int age;
   private String place;
+  
   public String getFirstName() {
     return firstName;
   }
   public void setFirstName(String firstName) {
     this.firstName = firstName;
   }
+  
   public String getLastName() {
     return lastName;
   }
   public void setLastName(String lastName) {
     this.lastName = lastName;
   }
+  
   public int getAge() {
     return age;
   }
   public void setAge(int age) {
     this.age = age;
   }
+  
   public String getPlace() {
     return place;
   }
@@ -30,14 +36,19 @@ public class Person {
     this.place = place;
   }
   
+  public int getId() {
+    return id;
+  }
+  public void setId(int id) {
+    this.id = id;
+  }
+  
   public String toString(){
     StringBuilder builder = new StringBuilder();
-    builder.append(this.getFirstName())
-      .append(", ")
-      .append(this.getLastName())
-      .append(", ")
-      .append(this.getPlace())
-      .append(", ")
+    builder.append(this.getId()).append(", ")
+      .append(this.getFirstName()).append(", ")
+      .append(this.getLastName()).append(", ")
+      .append(this.getPlace()).append(", ")
       .append(this.getAge());
     
     return builder.toString();
